edaa973c5c84cd6b3de7e3fe50d25a04b34222cf
parvathipriya90 <mpaarvathi90@gmail.com>
2025-01-09 05:46:19 +0530

    Test1 added

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+        public static void main(String[] args) {
+            char find = 'a';
+            Character[] n = {'1', 'a', '3',};
+            for (int i = 0; i < n.length; i++) {
+                if (n[i]==('a')) {
+                    System.out.println('a');
+                }
+            }
+        }
+    }
+
+
