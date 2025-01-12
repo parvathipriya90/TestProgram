c9b15f48baf48bf4fa26d2d2b74d6d9dc974b2e9
parvathipriya90 <mpaarvathi90@gmail.com>
2025-01-12 07:57:54 +0530

    ArrayVowels added

--- src/ArrayVowels.java
+++ src/ArrayVowels.java
@@ -1,26 +1,20 @@
 //[‘a’, ‘z’, ‘4’, ‘i’, ‘m’, ‘7’]
   //      input : char array
+//    collect the vowels, consonants, numbers into separate lists
 
 import java.util.Arrays;
-
-//    collect the vowels, consonants, numbers into separate lists
 public class ArrayVowels {
         public static void main(String[] args) {
-
+            char value=4;
             char[] array = {'a', 'z', '4', 'i', 'm', '7','e','u'};
-            char[] vowels = new char[4];
-            char[] consonants=new char[2];
-            char[] numbers=new char[2];
+            char[] vowels = new char[value];
+            char[] consonants=new char[value];
+            char[] numbers=new char[value];
             int vowelscount=0;
             int consonantscount=0;
             int numberscount=0;
             for (int number = 0; number<array.length;number++) {
-               // char current = array[number];
-                //if (isVowel(current)) {
-                  //  vowels[vowelCount++] = current;
-                //}
-
-                if (array[number]=='a'||array[number]=='i'||array[number]=='u'||array[number]=='e'){
+               if (array[number]=='a'||array[number]=='i'||array[number]=='u'||array[number]=='e'){
                     vowels[vowelscount++] = array[number];
             }else if(array[number]=='z'||array[number]=='m'){
                     consonants[consonantscount++]=array[number];
