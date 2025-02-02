e25e43eb60ca404bb40bc83e4ceb953080843a1b
parvathipriya90 <mpaarvathi90@gmail.com>
2025-02-01 20:05:19 -0500

    array find missing A

---
+++ src/ArrayFindMissinga.java
@@ -0,0 +1,11 @@
+public class ArrayFindMissinga {
+    public static void main(String[] args) {
+        char find='a';
+        Character[] array={'1','a','3'};
+        for(int i=0;i<array.length;i++){
+            if(array[i]==('a')){
+                System.out.println('a');
+            }
+        }
+    }
+}
