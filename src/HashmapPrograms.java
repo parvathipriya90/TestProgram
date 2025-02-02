86dbeb7898193262e4fcc464c17ed9134b3ba043
parvathipriya90 <mpaarvathi90@gmail.com>
2025-02-01 20:44:29 -0500

    HashmapPrograms

---
+++ src/HashmapPrograms.java
@@ -0,0 +1,12 @@
+public class HashmapPrograms {
+    public static void main(String[] args) {
+        ReverseCharacter reverseCharacter = new ReverseCharacter();
+        reverseCharacter.revChar("PARVATHI", "");
+       FindStarPattern starPattern = new FindStarPattern();
+        starPattern.starPattern(5);
+        FindReverseStar reverseStarPattern = new FindReverseStar();
+        reverseStarPattern.reverseStarPattern(5);
+
+    }
+}
+
